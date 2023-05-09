a73c6624c210cd62ebab142240f8f7725bb571d0
markgmz92 <markgmz92@gmail.com>
2023-05-09 21:39:55 +0800

    Created desktop navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Navbar from "@/scenes/navbar/index.tsx";
+import { useState } from "react";
+
 function App() {
-  return <div className="app bg-gray-20">App</div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
